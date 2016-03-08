a7c69feb6720aab6a998a9ea3a3fc35f972178b0
Andrew Buccilli <andrew.buccilli@cern.ch>
2016-03-08 17:59:56 -0600

    Script to chain ntuples to loop over.

--- FakeRateAnalysis/README.md
+++ FakeRateAnalysis/README.md
@@ -17,3 +17,5 @@ cd FakeRateAnalysis
 Merge ntuples from scripts directory: ./hadd_script.sh
 
 Make class from analysis directory: root ../scripts/diphoton_makeClass.C
+
+Chain ntuples from each root file to loop over from analysis directory: root ../scripts/diphoton_chain_looper.C
---
+++ FakeRateAnalysis/scripts/diphoton_chain_looper.C
@@ -0,0 +1,29 @@
+#include "FakeRateAnalysis.C"
+
+void diphoton_chain_looper() {
+  // use stopwatch to time
+  TStopwatch sw;
+  sw.Start();
+
+  // create tchain of all files to loop over
+  TChain *chain = new TChain("diphoton/fTree");
+  // wildcard not supported when using xrootd
+  // added ",0" to print correct number of entries, not entries=1234567890
+  chain->Add("root://cmsxrootd.fnal.gov//store/user/abuccill/DiPhotonAnalysis/FakeRateMerged/diphoton_fakeRate_JetHT_Run2015C_25ns-16Dec2015-v1_MINIAOD_merged.root",0);
+  chain->Add("root://cmsxrootd.fnal.gov//store/user/abuccill/DiPhotonAnalysis/FakeRateMerged/diphoton_fakeRate_JetHT_Run2015D-16Dec2015-v1_MINIAOD_merged.root",0);
+  chain->ls();
+  cout << "Total number of entries: " << chain->GetEntries() << endl; 
+
+  // create instance of class, passing our chain
+  FakeRateAnalysis ptl(chain);
+
+  // loop over all entries of our tree
+  ptl.Loop();
+
+  // stop stopwatch
+  sw.Stop();
+
+  // convert total time to minutes
+  std::cout << "RealTime : " << sw.RealTime() / 60.0 << " minutes" << std::endl;
+  std::cout << "CPUTime  : " << sw.CpuTime()  / 60.0 << " minutes" << std::endl;
+}
