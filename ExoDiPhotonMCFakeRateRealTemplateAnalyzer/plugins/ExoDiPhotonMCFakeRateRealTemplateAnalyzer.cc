9794c45fad13daf82ccf0203e1d8ed0dfd3e4859
Andrew Buccilli <andrew.buccilli@cern.ch>
2016-03-09 12:36:51 -0600

    Checked out new edanalyzer and added fake rate into to it.

---
+++ ExoDiPhotonMCFakeRateRealTemplateAnalyzer/plugins/ExoDiPhotonMCFakeRateRealTemplateAnalyzer.cc
@@ -0,0 +1,318 @@
+// -*- C++ -*-
+//
+// Package:    diphoton-analysis/ExoDiPhotonMCFakeRateRealTemplateAnalyzer
+// Class:      ExoDiPhotonMCFakeRateRealTemplateAnalyzer
+// 
+/**\class ExoDiPhotonMCFakeRateRealTemplateAnalyzer ExoDiPhotonMCFakeRateRealTemplateAnalyzer.cc diphoton-analysis/ExoDiPhotonMCFakeRateRealTemplateAnalyzer/plugins/ExoDiPhotonMCFakeRateRealTemplateAnalyzer.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Andrew Buccilli
+//         Created:  Wed, 09 Mar 2016 17:59:12 GMT
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+// from our CommomClasses
+#include "diphoton-analysis/CommonClasses/interface/EventInfo.h"
+#include "diphoton-analysis/CommonClasses/interface/JetInfo.h"
+#include "diphoton-analysis/CommonClasses/interface/PhotonID.h"
+#include "diphoton-analysis/CommonClasses/interface/PhotonInfo.h"
+
+// for TFileService, trees
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "TTree.h"
+
+// for ECAL topology
+#include "Geometry/CaloTopology/interface/CaloTopology.h"
+#include "Geometry/CaloEventSetup/interface/CaloTopologyRecord.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "DataFormats/EcalDetId/interface/EBDetId.h"
+#include "DataFormats/EcalDetId/interface/EEDetId.h"
+
+// for EGM ID
+#include "RecoEgamma/EgammaTools/interface/EffectiveAreas.h"
+
+// for photons
+#include "DataFormats/PatCandidates/interface/Photon.h"
+
+// for jets
+#include "DataFormats/PatCandidates/interface/Jet.h"
+
+//
+// class declaration
+//
+
+// If the analyzer does not use TFileService, please remove
+// the template argument to the base class so the class inherits
+// from  edm::one::EDAnalyzer<> and also remove the line from
+// constructor "usesResource("TFileService");"
+// This will improve performance in multithreaded jobs.
+
+class ExoDiPhotonMCFakeRateRealTemplateAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
+   public:
+      explicit ExoDiPhotonMCFakeRateRealTemplateAnalyzer(const edm::ParameterSet&);
+      ~ExoDiPhotonMCFakeRateRealTemplateAnalyzer();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() override;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+      virtual void endJob() override;
+
+  // ----------member data ---------------------------
+  // miniAOD photon token
+  edm::EDGetToken photonsMiniAODToken_;
+
+  // AK4 jet token and cuts
+  edm::EDGetToken jetsMiniAODToken_;
+  double jetPtThreshold;
+  double jetEtaThreshold;
+    
+  // ECAL recHits
+  edm::InputTag recHitsEBTag_;
+  edm::InputTag recHitsEETag_;
+  edm::EDGetTokenT<EcalRecHitCollection> recHitsEBToken;
+  edm::EDGetTokenT<EcalRecHitCollection> recHitsEEToken;
+
+  // rho token
+  edm::EDGetTokenT<double> rhoToken_;
+  
+  // rho variable
+  double rho_;
+
+  // EGM eff. areas
+  EffectiveAreas effAreaChHadrons_;
+  EffectiveAreas effAreaNeuHadrons_;
+  EffectiveAreas effAreaPhotons_;
+
+  // EGM ID decision tokens
+  edm::EDGetTokenT<edm::ValueMap<bool> > phoLooseIdMapToken_;
+  edm::EDGetTokenT<edm::ValueMap<bool> > phoMediumIdMapToken_;
+  edm::EDGetTokenT<edm::ValueMap<bool> > phoTightIdMapToken_;
+  
+  // trees
+  TTree *fTree;
+
+  // photons
+  ExoDiPhotons::photonInfo_t fPhotonInfo;
+
+  // jets
+  ExoDiPhotons::jetInfo_t fJetInfo;
+  
+  // event
+  ExoDiPhotons::eventInfo_t fEventInfo;
+  
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+ExoDiPhotonMCFakeRateRealTemplateAnalyzer::ExoDiPhotonMCFakeRateRealTemplateAnalyzer(const edm::ParameterSet& iConfig)
+  : rhoToken_(consumes<double> (iConfig.getParameter<edm::InputTag>("rho"))),
+    effAreaChHadrons_( (iConfig.getParameter<edm::FileInPath>("effAreaChHadFile")).fullPath() ),
+    effAreaNeuHadrons_( (iConfig.getParameter<edm::FileInPath>("effAreaNeuHadFile")).fullPath() ),
+    effAreaPhotons_( (iConfig.getParameter<edm::FileInPath>("effAreaPhoFile")).fullPath() ),
+    phoLooseIdMapToken_(consumes<edm::ValueMap<bool> >(iConfig.getParameter<edm::InputTag>("phoLooseIdMap"))),
+    phoMediumIdMapToken_(consumes<edm::ValueMap<bool> >(iConfig.getParameter<edm::InputTag>("phoMediumIdMap"))),
+    phoTightIdMapToken_(consumes<edm::ValueMap<bool> >(iConfig.getParameter<edm::InputTag>("phoTightIdMap")))
+{
+  //now do what ever initialization is needed
+   usesResource("TFileService");
+
+   edm::Service<TFileService> fs;
+
+  // tree for objects passing numerator or denominator definitions
+  fTree = fs->make<TTree>("fTree","PhotonTree");
+  fTree->Branch("Event",&fEventInfo,ExoDiPhotons::eventBranchDefString.c_str());
+  fTree->Branch("Jet",&fJetInfo,ExoDiPhotons::jetBranchDefString.c_str());
+  fTree->Branch("Photon",&fPhotonInfo,ExoDiPhotons::photonBranchDefString.c_str());
+  
+  // MiniAOD tokens
+  photonsMiniAODToken_ = mayConsume<edm::View<pat::Photon> >(iConfig.getParameter<edm::InputTag>("photonsMiniAOD"));
+
+  // AK4 jets token
+  jetsMiniAODToken_ = mayConsume< edm::View<pat::Jet> >(iConfig.getParameter<edm::InputTag>("jetsMiniAOD"));
+  jetPtThreshold = iConfig.getParameter<double>("jetPtThreshold");
+  jetEtaThreshold = iConfig.getParameter<double>("jetEtaThreshold");
+  
+  // ECAL RecHits
+  recHitsEBTag_ = iConfig.getUntrackedParameter<edm::InputTag>("RecHitsEBTag",edm::InputTag("reducedEgamma:reducedEBRecHits"));
+  recHitsEETag_ = iConfig.getUntrackedParameter<edm::InputTag>("RecHitsEETag",edm::InputTag("reducedEgamma:reducedEERecHits"));
+  recHitsEBToken = consumes <edm::SortedCollection<EcalRecHit> > (recHitsEBTag_);
+  recHitsEEToken = consumes <edm::SortedCollection<EcalRecHit> > (recHitsEETag_);
+}
+
+
+ExoDiPhotonMCFakeRateRealTemplateAnalyzer::~ExoDiPhotonMCFakeRateRealTemplateAnalyzer()
+{
+ 
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void
+ExoDiPhotonMCFakeRateRealTemplateAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  using namespace edm;
+  using namespace std;
+  using namespace pat;
+  
+  ExoDiPhotons::InitEventInfo(fEventInfo);
+  ExoDiPhotons::FillBasicEventInfo(fEventInfo, iEvent);
+  
+  cout <<  "Run: " << iEvent.id().run() << ", LS: " <<  iEvent.id().luminosityBlock() << ", Event: " << iEvent.id().event() << endl;
+  
+  ExoDiPhotons::InitJetInfo(fJetInfo);
+  
+  // add jet HT information
+  edm::Handle< edm::View<pat::Jet> > jets;
+  iEvent.getByToken(jetsMiniAODToken_,jets);
+  
+  int nJets = 0;
+  double jetHT = 0.;
+  for (auto &j : *jets){
+    double jetPt = j.pt();
+    double jetEta = fabs( j.eta() );
+    if (jetPt > jetPtThreshold && jetEta < jetEtaThreshold ){
+      nJets++;
+      jetHT += jetPt;
+    }
+  }
+  fJetInfo.nJets = nJets;
+  fJetInfo.jetHT = jetHT;
+  
+  // Get rho
+  edm::Handle< double > rhoH;
+  iEvent.getByToken(rhoToken_,rhoH);
+  rho_ = *rhoH;
+  
+  cout << "rho: " << rho_ << endl;
+  
+  // EGM ID decisions
+  edm::Handle<edm::ValueMap<bool> > loose_id_decisions;
+  iEvent.getByToken(phoLooseIdMapToken_ ,loose_id_decisions);
+  edm::Handle<edm::ValueMap<bool> > medium_id_decisions;
+  iEvent.getByToken(phoMediumIdMapToken_,medium_id_decisions);
+  edm::Handle<edm::ValueMap<bool> > tight_id_decisions;
+  iEvent.getByToken(phoTightIdMapToken_ ,tight_id_decisions);
+  
+  // ECAL RecHits
+  edm::Handle<EcalRecHitCollection> recHitsEB;
+  iEvent.getByToken(recHitsEBToken,recHitsEB);   
+  edm::Handle<EcalRecHitCollection> recHitsEE;
+  iEvent.getByToken(recHitsEEToken,recHitsEE);
+
+  // ECAL Topology
+  const CaloSubdetectorTopology* subDetTopologyEB_;
+  const CaloSubdetectorTopology* subDetTopologyEE_;
+  edm::ESHandle<CaloTopology> caloTopology;
+  iSetup.get<CaloTopologyRecord>().get(caloTopology);
+  subDetTopologyEB_ = caloTopology->getSubdetectorTopology(DetId::Ecal,EcalBarrel);
+  subDetTopologyEE_ = caloTopology->getSubdetectorTopology(DetId::Ecal,EcalEndcap);
+
+  ExoDiPhotons::InitPhotonInfo(fPhotonInfo);
+  
+  // Get pat::Photon collection
+  edm::Handle<edm::View<pat::Photon> > photons;
+  iEvent.getByToken(photonsMiniAODToken_,photons);
+  
+  //for (edm::View<pat::Photon>::const_iterator pho = photons->begin(); pho != photons->end(); ++pho) {
+  for (size_t i = 0; i < photons->size(); ++i){
+    const auto pho = photons->ptrAt(i);
+
+    // print photon info
+    cout << "Photon: " << "pt = " << pho->pt() << "; eta = " << pho->eta() << "; phi = " << pho->phi() << endl;
+    
+    // fill photon saturation
+    fPhotonInfo.isSaturated = ExoDiPhotons::isSaturated(&(*pho), &(*recHitsEB), &(*recHitsEE), &(*subDetTopologyEB_), &(*subDetTopologyEE_));
+    cout << "isSat: " << fPhotonInfo.isSaturated << endl;
+
+    // fill photon info
+    ExoDiPhotons::FillBasicPhotonInfo(fPhotonInfo, &(*pho));
+    ExoDiPhotons::FillPhotonIDInfo(fPhotonInfo, &(*pho), rho_, fPhotonInfo.isSaturated);
+    
+    // fill EGM ID info
+    ExoDiPhotons::FillPhotonEGMidInfo(fPhotonInfo, &(*pho), rho_, effAreaChHadrons_, effAreaNeuHadrons_, effAreaPhotons_);
+    fPhotonInfo.passEGMLooseID  = (*loose_id_decisions)[pho];
+    fPhotonInfo.passEGMMediumID = (*medium_id_decisions)[pho];
+    fPhotonInfo.passEGMTightID  = (*tight_id_decisions)[pho];
+
+    // fill our tree
+    if ( ExoDiPhotons::passLooseNumeratorCut(&(*pho), rho_, fPhotonInfo.isSaturated) ||
+         ExoDiPhotons::passDenominatorCut(&(*pho), rho_, fPhotonInfo.isSaturated) ||
+         fPhotonInfo.isFakeTemplateObjCand // this was already filled with ExoDiPhotons::FillPhotonIDInfo
+       ) fTree->Fill();
+										  
+  } // end of photon loop
+
+
+#ifdef THIS_IS_AN_EVENT_EXAMPLE
+   Handle<ExampleData> pIn;
+   iEvent.getByLabel("example",pIn);
+#endif
+   
+#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+   ESHandle<SetupData> pSetup;
+   iSetup.get<SetupRecord>().get(pSetup);
+#endif
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+ExoDiPhotonMCFakeRateRealTemplateAnalyzer::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+ExoDiPhotonMCFakeRateRealTemplateAnalyzer::endJob() 
+{
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+ExoDiPhotonMCFakeRateRealTemplateAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(ExoDiPhotonMCFakeRateRealTemplateAnalyzer);
---
+++ ExoDiPhotonMCFakeRateRealTemplateAnalyzer/test/diphoton_fakeRate_cfg.py
@@ -0,0 +1,79 @@
+# runtime options
+
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing ('python')
+
+options.register('globalTag',
+                '76X_dataRun2_v15',
+                VarParsing.multiplicity.singleton,
+                VarParsing.varType.string,
+                "global tag to use when running"
+)
+## 'maxEvents' is already registered by the Framework, changing default value
+options.setDefault('maxEvents', 100)
+
+options.parseArguments()
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("ExoDiPhoton")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32( options.maxEvents ) )
+
+# for input file
+process.source = cms.Source(
+    "PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        #'file:myfile.root'
+        'root://cmsxrootd.fnal.gov//store/data/Run2015C_25ns/JetHT/MINIAOD/16Dec2015-v1/20000/B8EFF9C3-48B5-E511-87B3-3417EBE64444.root'
+        )
+    )
+
+# for global tag
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = options.globalTag
+
+# geometry for saturation
+process.load("Configuration.StandardSequences.GeometryDB_cff")
+
+# for output file
+process.TFileService = cms.Service(
+    "TFileService",
+    fileName = cms.string("ExoDiphotonAnalyzer.root")
+    )
+
+# Setup VID for EGM ID
+from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
+switchOnVIDPhotonIdProducer(process, DataFormat.MiniAOD)
+# define which IDs we want to produce
+my_id_modules = ['RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonID_Spring15_25ns_V1_cff']
+#add them to the VID producer
+for idmod in my_id_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDPhotonSelection)
+
+# analyzer and inputs
+process.diphoton = cms.EDAnalyzer(
+    'ExoDiPhotonFakeRateAnalyzer',
+    # photon tag
+    photonsMiniAOD = cms.InputTag("slimmedPhotons"),
+    # ak4 jets
+    jetsMiniAOD = cms.InputTag("slimmedJets"),
+    jetPtThreshold = cms.double(30.),
+    jetEtaThreshold = cms.double(2.5),
+    # rho tag (use fixedGridRhoAll?)
+    rho = cms.InputTag("fixedGridRhoFastjetAll"),
+    # EGM eff. areas
+    effAreaChHadFile = cms.FileInPath("RecoEgamma/PhotonIdentification/data/Spring15/effAreaPhotons_cone03_pfChargedHadrons_25ns_NULLcorrection.txt"),
+    effAreaNeuHadFile = cms.FileInPath("RecoEgamma/PhotonIdentification/data/Spring15/effAreaPhotons_cone03_pfNeutralHadrons_25ns_90percentBased.txt"),
+    effAreaPhoFile = cms.FileInPath("RecoEgamma/PhotonIdentification/data/Spring15/effAreaPhotons_cone03_pfPhotons_25ns_90percentBased.txt"),
+    # EGM ID decisions
+    phoLooseIdMap = cms.InputTag("egmPhotonIDs:cutBasedPhotonID-Spring15-25ns-V1-standalone-loose"),
+    phoMediumIdMap = cms.InputTag("egmPhotonIDs:cutBasedPhotonID-Spring15-25ns-V1-standalone-medium"),
+    phoTightIdMap = cms.InputTag("egmPhotonIDs:cutBasedPhotonID-Spring15-25ns-V1-standalone-tight"),
+    )
+
+process.p = cms.Path(process.egmPhotonIDSequence * process.diphoton)
