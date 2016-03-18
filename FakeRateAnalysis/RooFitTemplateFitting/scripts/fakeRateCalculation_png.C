84ee6dce5a4627b4ea4c0d8f11e5b08c56ba746d
Steven Kaplan <skaplan@physics.rutgers.edu>
2016-03-18 08:41:17 -0500

    roofit scrips that output in png, update .gitignore

---
+++ FakeRateAnalysis/RooFitTemplateFitting/scripts/fakeRateCalculation_png.C
@@ -0,0 +1,62 @@
+#include "rooFitFakeRateProducer_pTBinLoop_png.C"
+
+void fakeRateCalculation() {
+  // use stopwatch to time
+  TStopwatch sw;
+  sw.Start();
+
+  int ptBinArray[17] = { 30, 50, 70, 90, 110, 130, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 14000 };
+
+  TGraphAsymmErrors fakeRateEB;
+  TGraphAsymmErrors fakeRateEE;
+  fakeRateEB.SetName("fakeRateEB");
+  fakeRateEE.SetName("fakeRateEE");
+
+  TFile outfile("fakeRatePlots.root","recreate");
+  outfile.Close();
+  // loop over pT bins
+  for (int i = 0; i < 16; i++){
+    if (i == 15) continue; // no 600ToInf real template yet
+    TString binName = TString::Format("%iTo%i",ptBinArray[i],ptBinArray[i+1]);
+    // run calculation twice, once for EB and once for EE
+    std::pair<double,double> resEB = rooFitFakeRateProducer(binName,TString("EB"),i+1); // i+1 is the bin number in the denominator pT distribution corresponding to this pT bin
+    std::pair<double,double> resEE = rooFitFakeRateProducer(binName,TString("EE"),i+1);
+
+    // record fake rate in TGraphs
+    double graphX = (1.*ptBinArray[i] + 1.*ptBinArray[i+1])/2.;
+    double graphY_EB = resEB.first;
+    double graphY_EE = resEE.first;
+    double ex = (1.*ptBinArray[i+1] - 1.*ptBinArray[i])/2.;
+    fakeRateEB.SetPoint(i,graphX,graphY_EB);
+    fakeRateEB.SetPointError(i,ex,ex,0.,0.);
+    fakeRateEE.SetPoint(i,graphX,graphY_EE);
+    fakeRateEE.SetPointError(i,ex,ex,0.,0.);
+  }
+  TFile outfile2("fakeRatePlots.root","update");
+  outfile2.cd();
+  fakeRateEB.Write();
+  fakeRateEE.Write();
+  outfile2.Close();
+
+  TCanvas c("c","",1200,800);
+  fakeRateEB.Draw("AL");
+  fakeRateEB.GetXaxis()->SetTitle("Photon pT (GeV/c)");
+  fakeRateEB.GetYaxis()->SetTitle("Fake Rate");
+  fakeRateEB.Draw("AL");
+  c.SaveAs("fakeRateEB.png");
+
+  c.Clear();
+  fakeRateEE.Draw("AL");
+  fakeRateEE.GetXaxis()->SetTitle("Photon pT (GeV/c)");
+  fakeRateEE.GetYaxis()->SetTitle("Fake Rate");
+  fakeRateEE.GetYaxis()->SetTitleOffset(1.4);
+  fakeRateEE.Draw("AL");
+  c.SaveAs("fakeRateEE.png");
+
+  // stop stopwatch
+  sw.Stop();
+
+  // convert total time to minutes
+  std::cout << "RealTime : " << sw.RealTime() / 60.0 << " minutes" << std::endl;
+  std::cout << "CPUTime  : " << sw.CpuTime()  / 60.0 << " minutes" << std::endl;
+}
