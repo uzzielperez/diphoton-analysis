a795699c67b93f9269a2ad9e79fe6b6dcb41576e
Andrew Buccilli <andrew.buccilli@cern.ch>
2016-03-29 08:47:38 -0500

    Sieie plots for closure test

--- FakeRateAnalysis/MCFakeRateClosureTest/analysis/MCFakeRateClosureTest.C
+++ FakeRateAnalysis/MCFakeRateClosureTest/analysis/MCFakeRateClosureTest.C
@@ -4,7 +4,7 @@
 #include <TStyle.h>
 #include <TCanvas.h>
 
-void MCFakeRateClosureTest::Loop()
+void MCFakeRateClosureTest::Loop(const Char_t *iMass)
 {
 //   In a ROOT session, you can do:
 //      root> .L MCFakeRateClosureTest.C
@@ -31,13 +31,112 @@ void MCFakeRateClosureTest::Loop()
 //by  b_branchname->GetEntry(ientry); //read only this branch
    if (fChain == 0) return;
 
+   vector<TH1D*> sigmaIetaIetaEB_pt20;  
+   for (int i = 30; i<140; i=i+20) {
+     TH1D *h = new TH1D(Form("sieieEB_mcFake_pt%dTo%d",i,i+20),"sigmaIetaIetaEB",200,0.,0.1);
+     h->Sumw2();
+     sigmaIetaIetaEB_pt20.push_back(h);
+   }
+   vector<TH1D*> sigmaIetaIetaEB_pt50;  
+   for (int i = 150; i<650; i=i+50) {
+     TH1D *h = new TH1D(Form("sieieEB_mcFake_pt%dTo%d",i,i+50),"sigmaIetaIetaEB",200,0.,0.1);
+     h->Sumw2();
+     sigmaIetaIetaEB_pt50.push_back(h);
+   }
+   
+   vector<TH1D*> sigmaIetaIetaEE_pt20;  
+   for (int i = 30; i<140; i=i+20) {
+     TH1D *h = new TH1D(Form("sieieEE_mcFake_pt%dTo%d",i,i+20),"sigmaIetaIetaEE",200,0.,0.1);
+     h->Sumw2();
+     sigmaIetaIetaEE_pt20.push_back(h);
+   }
+   vector<TH1D*> sigmaIetaIetaEE_pt50;  
+   for (int i = 150; i<650; i=i+50) {
+     TH1D *h = new TH1D(Form("sieieEE_mcFake_pt%dTo%d",i,i+50),"sigmaIetaIetaEE",200,0.,0.1);
+     h->Sumw2();
+     sigmaIetaIetaEE_pt50.push_back(h);
+   }
+   
    Long64_t nentries = fChain->GetEntriesFast();
-
    Long64_t nbytes = 0, nb = 0;
    for (Long64_t jentry=0; jentry<nentries;jentry++) {
-      Long64_t ientry = LoadTree(jentry);
-      if (ientry < 0) break;
-      nb = fChain->GetEntry(jentry);   nbytes += nb;
-      // if (Cut(ientry) < 0) continue;
+     Long64_t ientry = LoadTree(jentry);
+     if (ientry < 0) break;
+     nb = fChain->GetEntry(jentry);   nbytes += nb;
+     // if (Cut(ientry) < 0) continue;
+     if (jentry % 100000 == 0)
+       std::cout << "Number of entries looped over: " << jentry << std::endl;
+      
+     //if (Event_beamHaloIDTight2015) continue;
+     if (Photon_sigmaIphiIphi5x5 < 0.009) continue;
+
+     // only select photon matched to gluons or quarks (no photon matches are stored)
+     //if (PhotonGenMatch_pdgId != 21 && fabs(PhotonGenMatch_pdgId) > 7) continue;
+
+     // EB
+     if (fabs(Photon_scEta) < 1.4442) {
+       for (int i = 30; i<140; i=i+20) {
+	 if (i < Photon_pt && Photon_pt < i+20) {
+	   if (Photon_isNumeratorObjCand && Photon_passChIso) {
+	     sigmaIetaIetaEB_pt20[(i-30)/20]->Fill(Photon_sigmaIetaIeta5x5,Event_weight);
+	   }
+	 }
+       }
+       for (int i = 150; i<650; i=i+50) {
+	 if (i < Photon_pt && Photon_pt < i+50) {
+	   if (Photon_isNumeratorObjCand && Photon_passChIso) {
+	     sigmaIetaIetaEB_pt50[(i-150)/50]->Fill(Photon_sigmaIetaIeta5x5,Event_weight);
+	   }
+	 }
+       }
+     } // end EB
+      
+     // EE
+     if (1.566 < fabs(Photon_scEta) && fabs(Photon_scEta) < 2.5) {
+       for (int i = 30; i<140; i=i+20) {
+	 if (i < Photon_pt && Photon_pt < i+20) {
+	   if (Photon_isNumeratorObjCand && Photon_passChIso) {
+	     sigmaIetaIetaEE_pt20[(i-30)/20]->Fill(Photon_sigmaIetaIeta5x5,Event_weight);
+	   }
+	 }
+       }
+       for (int i = 150; i<650; i=i+50) {
+	 if (i < Photon_pt && Photon_pt < i+50) {
+	   if (Photon_isNumeratorObjCand && Photon_passChIso) {
+	     sigmaIetaIetaEE_pt50[(i-150)/50]->Fill(Photon_sigmaIetaIeta5x5,Event_weight);
+	   }
+	 }
+       }
+     } // end EE
+      
+   } // end loop over entries
+
+   TString filename;
+   filename = TString::Format("diphoton_fakeRate_QCD_Pt-%s_EMEnriched_TuneCUETP8M1_13TeV_pythia8_76X_MiniAOD_histograms.root",iMass);
+   TFile file_out(filename,"RECREATE");
+
+   for (vector<TH1D*>::iterator it = sigmaIetaIetaEB_pt20.begin() ; it != sigmaIetaIetaEB_pt20.end(); ++it) {
+     (*it)->Scale(1.0/(*it)->Integral());
+     (*it)->Write();
+     //cout << (*it)->GetName() << "\t integral: " << (*it)->Integral() << endl;
    }
-}
+   for (vector<TH1D*>::iterator it = sigmaIetaIetaEB_pt50.begin() ; it != sigmaIetaIetaEB_pt50.end(); ++it) {
+     (*it)->Scale(1.0/(*it)->Integral());
+     (*it)->Write();
+     //cout << (*it)->GetName() << "\t integral: " << (*it)->Integral() << endl;
+   }
+   for (vector<TH1D*>::iterator it = sigmaIetaIetaEE_pt20.begin() ; it != sigmaIetaIetaEE_pt20.end(); ++it) {
+     (*it)->Scale(1.0/(*it)->Integral());
+     (*it)->Write();
+     //cout << (*it)->GetName() << "\t integral: " << (*it)->Integral() << endl;
+   }
+   for (vector<TH1D*>::iterator it = sigmaIetaIetaEE_pt50.begin() ; it != sigmaIetaIetaEE_pt50.end(); ++it) {
+     (*it)->Scale(1.0/(*it)->Integral());
+     (*it)->Write();
+     //cout << (*it)->GetName() << "\t integral: " << (*it)->Integral() << endl;
+   }
+   
+   file_out.ls();
+   file_out.Close();
+   
+} // end of Loop()
