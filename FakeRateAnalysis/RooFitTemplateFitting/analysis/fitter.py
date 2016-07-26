451648e0103789d518e570be5edfb74b4ae31aad
Steven Kaplan <skaplan@physics.rutgers.edu>
2016-07-26 16:55:04 -0500

    add fake rate fit script

---
+++ FakeRateAnalysis/RooFitTemplateFitting/analysis/fitter.py
@@ -0,0 +1,99 @@
+print "importing ROOT..."
+from ROOT import *
+import numpy as np
+print "done."
+
+gROOT.SetBatch()
+
+def TGraphAsymmErrorsToTH1D(g,histName):
+    numPoints = g.GetN()
+    # xHigh = g.GetErrorXhigh(numPoints-1)
+    # xLow = g.GetErrorXlow(0)
+    binBoundaries = []
+    binContents = []
+    binErrors = []
+    for i in range(numPoints):
+        x = Double()
+        y = Double()
+        g.GetPoint(i,x,y)
+        # print "got point %i %.5f %.5f"%(i,x,y)
+        binBoundaries.append( x - g.GetErrorXlow(i) )
+        # print "appended %.5f"%( x - g.GetErrorXlow(i) )
+        binContents.append( y )
+        binErrors.append( g.GetErrorYlow(i) ) #low and high are equal, just picked one
+        if i == numPoints-1:
+            # binBoundaries.append(x + g.GetErrorXhigh(i)) #get last value also
+            binBoundaries.append(600.) #hack because SetRangeUser wasn't working...
+            # print "appended %.5f"%(x + g.GetErrorXhigh(i))
+    binBoundaries = np.array(binBoundaries)
+    h = TH1D(histName,"",len(binContents),binBoundaries)
+    for i in range(numPoints):
+        h.SetBinContent(i+1,binContents[i])
+        h.SetBinError(i+1,binErrors[i])
+    return h
+
+gStyle.SetOptFit(11111111)
+
+infile = TFile("./jethtvanillaplots/fakeRatePlots.root","read")
+
+plotorigEB = infile.Get("fakeRateEB_chIso9To14")
+plotEB = plotorigEB.Clone()
+
+plotorigEE = infile.Get("fakeRateEE_chIso9To14")
+plotEEclone = plotorigEE.Clone()
+plotEE = TGraphAsymmErrorsToTH1D(plotEEclone,"plotEE")
+
+EBfunc = TF1("EBfunc","[0]+[1]/(x^[2])",0.,600.)
+EBfunc.SetParameter(0,0.2)
+EBfunc.SetParameter(1,1.)
+EBfunc.SetParameter(2,1.)
+
+print "********EB FIT************"
+resEB = plotEB.Fit(EBfunc,"RS","",0.,600.)
+print "chi**2 = %.4f"%resEB.Chi2()
+print "ndf = %i"%resEB.Ndf()
+
+print "********EE FIT************"
+EEfuncHigh = TF1("EEfuncHigh","pol1",170.,600.)
+resHigh = plotEE.Fit(EEfuncHigh,"SR")
+EEfuncLow = TF1("EEfuncLow","[0]+[1]*(x-175)+[2]*(x-175)^2",45.,180.)
+EEfuncLow.FixParameter(0,EEfuncHigh.Eval(175.)) # high pT linear fit evaluated at 175
+EEfuncLow.FixParameter(1,EEfuncHigh.GetParameter(1)) # slope of high pT linear fit (to match derivatives at pT=175)
+resLow = plotEE.Fit(EEfuncLow,"SR+")
+
+print "*******EB FIT RESULTS**********"
+print "Fit Function: [0]+[1]/(x^[2])"
+for i in range(3):
+    print "Param %i = %.14f"%(i,EBfunc.GetParameter(i))
+print "chi^2 = %.4f"%resEB.Chi2()
+print "ndf = %i"%resEB.Ndf()
+
+
+print "*******EE FIT RESULTS**********"
+print "Fit Function Below 175 GeV: [0]+[1]*(x-175)+[2]*(x-175)^2"
+for i in range(3):
+    print "Param %i = %.14f"%(i,EEfuncLow.GetParameter(i))
+print "chi^2 = %.4f"%resLow.Chi2()
+print "ndf = %.4f"%resLow.Ndf()
+print "Fit Function Above 175 GeV: pol1"
+print "slope = %.14f"%EEfuncHigh.GetParameter(1)
+print "yint = %.14f"%EEfuncHigh.GetParameter(0)
+print "chi^2 = %.4f"%resHigh.Chi2()
+print "ndf = %.4f"%resHigh.Ndf()
+
+for f in (plotEB,plotEE):
+    f.GetXaxis().SetTitle("pT (GeV)")
+    f.GetYaxis().SetTitle("Fake Rate")
+
+# gStyle.SetOptStat(111111)
+c = TCanvas("c","",1200,800)
+c.cd()
+plotEB.Draw()
+c.SaveAs("fitEB.pdf")
+
+c.Clear()
+plotEE.Draw()
+c.SaveAs("fitEE.pdf")
+
+
+# raw_input("enter to quit: ")
