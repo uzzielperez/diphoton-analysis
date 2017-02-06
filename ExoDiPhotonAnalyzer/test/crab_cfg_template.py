365c95ffb6ac5311b9fa7dade63a6b4315722b8c
Chris West <christopher.alan.west@gmail.com>
2017-02-06 10:29:07 +0100

    Fix lumi-masks

--- ExoDiPhotonAnalyzer/test/crab_cfg_template.py
+++ ExoDiPhotonAnalyzer/test/crab_cfg_template.py
@@ -34,11 +34,11 @@ if "Run2016" in taskname:
     config.Data.splitting = 'LumiBased'
     config.Data.unitsPerJob = 100
     if "Prompt" in taskname:
-        config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/Cert_271036-282092_13TeV_PromptReco_Collisions16_JSON.txt'
+        config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/Final/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.txt'
     # otherwise, assume Re-Reco
     else:
         config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
-if "Run2015" in taskname:
+elif "Run2015" in taskname:
     config.Data.splitting = 'LumiBased'
     config.Data.unitsPerJob = 100
     # only use 16Dec2015 ReReco
@@ -46,7 +46,6 @@ if "Run2015" in taskname:
 else:
     config.Data.splitting = 'FileBased'
     config.Data.unitsPerJob = 5
-
 
 config.section_("Site")
 config.Site.storageSite = 'T2_CH_CERN'
