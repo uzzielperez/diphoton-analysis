72a14056cebdf10268cc305435a8c28117c98042
Uzziel <uzzie.perez@cern.ch>
2019-02-07 10:53:15 -0600

    Taskname shortening

--- ExoDiPhotonAnalyzer/test/crab_cfg_template.py
+++ ExoDiPhotonAnalyzer/test/crab_cfg_template.py
@@ -33,6 +33,8 @@ config.JobType.pyCfgParams = ['nEventsSample=' + str(nevents), 'outputFile=out_'
 config.section_("Data")
 config.Data.inputDataset = dataset
 config.Data.inputDBS = 'global'
+config.Data.outLFNDirBase = '/store/user/ciperez/DiPhotonAnalysis/'
+
 if "Run2018" in taskname:
     config.Data.splitting = 'LumiBased'
     config.Data.unitsPerJob = 10
