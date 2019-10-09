3339dddf2e172392663aa67b7c4e2736aef53f24
Chris West <christopher.alan.west@gmail.com>
2019-10-09 13:37:09 -0500

    Use consistent binning

--- Tools/interface/fakePrediction.C
+++ Tools/interface/fakePrediction.C
@@ -95,9 +95,9 @@ void fakePrediction::Loop(int year, const std::string &dataset)
   const std::vector<std::string> regions = {"BB", "BE"};
 
   // define binning for input to datacard
-  int nbins=40;
-  double xmin=0.0;
-  double xmax=2000.0;
+  int nbins = 240;
+  double xmin = 0.0;
+  double xmax = 6000.0;
 
   setTDRStyle();
   gROOT->ForceStyle();
