caefbb054111d3f3c8d479096b2bbf1736d97278
Uzziel <uzzie.perez@cern.ch>
2020-08-17 06:38:33 -0500

    Print out fake rate in legacy code

--- FakeRateAnalysis/ClosureTestTemplateFits/fit.C
+++ FakeRateAnalysis/ClosureTestTemplateFits/fit.C
@@ -37,10 +37,13 @@ TH1* GetHist(int ptbin, bool isEB, hist_t type)
 {
   rootfile=0;
   if(type==Real)
+    // Output from RealTemplateAnalysis/analysis/MCFakeRateAnalysis.C
     rootfile=new TFile("diphoton_fake_rate_real_templates_all_GGJets_GJets_76X_MiniAOD_histograms.root");
   else if(type==Truth)
-    rootfile=new TFile("diphoton_fake_rate_closure_test_matching_all_samples_76X_MiniAOD_histograms.root");
+    // Output from PhotonClosure/analysis/MCFakeRateClosureTestWithFakes2.C
+    rootfile=new TFile("diphoton_fake_rate_closure_test_matching_all_samples_80X_MiniAOD_histograms_test.root");
   else
+    // Output from PhotonClosure/analysis/MCFakeRateAnalysis.C
     rootfile=new TFile("diphoton_fake_rate_closure_test_all_samples_76X_MiniAOD_histograms.root");
   assert(rootfile);
   rootfile->cd();
@@ -56,7 +59,7 @@ TH1* GetHist(int ptbin, bool isEB, hist_t type)
     oss << "Pt" << (isEB ? "EB" : "EE") << "_denominator_pt" << PTBINS[ptbin] << "To" << PTBINS[ptbin+1];
   else if(type==Truth)
     oss << "sieie" << (isEB ? "EB" : "EE") << "_numerator_fakes_pt" << PTBINS[ptbin] << "To" << PTBINS[ptbin+1];
-  
+
   TH1* h=dynamic_cast<TH1*>( gROOT->FindObject(oss.str().c_str()) );
   if(!h) {
     cout << "Cannot find object " << oss.str().c_str() << " (" << ptbin << ", " << isEB << ", " << type << ")" << endl;
@@ -69,7 +72,7 @@ TH1* GetHist(int ptbin, bool isEB, hist_t type)
     else
       h=h->Rebin(NEEBINS, TString(h->GetName())+"_rebin", EEBINS);
   }
-  
+
   return h;
 }
 
@@ -80,7 +83,7 @@ void NLL(int &, double *, double &f, double *par, int)
   double frac=par[0];
   double norm1=theData->Integral()/theBkg1->Integral();
   double norm2=theData->Integral()/theBkg2->Integral();
-  
+
   f=0.0;
   for(int i=1; i<=theData->GetNbinsX(); i++) {
     double obs=theData->GetBinContent(i);
@@ -130,7 +133,7 @@ int performFit(TH1* hData, TH1* hBkg1, TH1* hBkg2, double &frac, double &frace)
 
   // set the error to the symmetrized minos errors
   frace=(eplus-eminus)/2.0;
-  
+
   return err;
 }
 
@@ -153,19 +156,19 @@ void fit(void)
   double EETruth[NPTBINS], EETruthE[NPTBINS];
   int EBTruthStatus[NPTBINS], EETruthStatus[NPTBINS];
 
-  
+  // Template Fit per pT bin
   for(int isTruth=0; isTruth<2; isTruth++) {
     for(int isEB=0; isEB<2; isEB++) {
       for(int ptbin=0; ptbin<NPTBINS; ptbin++) {
-	
+
 	TH1* hReal=GetHist(ptbin, isEB, Real);
 	TH1* hFake= isTruth ? GetHist(ptbin, isEB, Truth) : GetHist(ptbin, isEB, Fake);
 	TH1* hNum=GetHist(ptbin, isEB, Numerator);
 	TH1* hDem=GetHist(ptbin, isEB, Denominator);
-	
+
 	double frac, frace;
 	int err=performFit(hNum, hReal, hFake, frac, frace);
-	
+
 	double norm1=hNum->Integral()/hReal->Integral();
 	double norm2=hNum->Integral()/hFake->Integral();
 	hReal->Scale(norm1*frac);
@@ -174,13 +177,13 @@ void fit(void)
 	hReal->SetFillStyle(3001);
 	hFake->SetFillColor(kRed);
 	hFake->SetFillStyle(3002);
-	
+
 	ostringstream oss;
 	oss << "h" << PTBINS[ptbin] << "To" << PTBINS[ptbin+1] << "_" << (isEB ? "EB" : "EE") << "_" << (isTruth ? "Truth" : "Fake");
 	THStack *hs=new THStack(oss.str().c_str(), "");
 	hs->Add(hFake, "HIST");
 	hs->Add(hReal, "HIST");
-	
+
 	if(isTruth) {
 	  if(isEB) cEBTruth->cd(ptbin+1);
 	  else     cEETruth->cd(ptbin+1);
@@ -192,7 +195,8 @@ void fit(void)
 	hNum->Draw("same");
 	gPad->SetLogy(1);
 	hs->GetXaxis()->SetTitle("#sigma_{i#etai#eta}");
-	
+
+  // Fake Rate Part
 	double num=0.0, nume2=0.0;
 	int nbins=isEB ? EBSIEIECUTBIN-1 : EESIEIECUTBIN-1;
 	for(int i=1; i<=nbins; i++) {
@@ -222,6 +226,29 @@ void fit(void)
   for(int i=0; i<NPTBINS; i++)
     std::cout << "Truth EE " << i << ": " << EETruth[i] << " +/- " << EETruthE[i] << " (" << EETruthE[i]/EETruth[i]*100 << "%; status=" << EETruthStatus[i] << ")" << std::endl;
 
+  // Fake Rate
+  TCanvas *cEBFakeRate=new TCanvas("cEBFakeRate", "EBFakeRate");
+  TCanvas *cEEFakeRate=new TCanvas("cEEFakeRate", "EEFakeRate");
+  TH1D* hEBFakeRate=new TH1D("hEBFakeRate", "EB FakeRate", NPTBINS, PTBINS);
+  TH1D* hEEFakeRate=new TH1D("hEEFakeRate", "EE FakeRate", NPTBINS, PTBINS);
+
+  for(int i=0; i<NPTBINS; i++) {
+    std::cout << "EB FakeRate " << i << ": " << EBFake[i]  << " +/- " << EBFakeE[i] << std::endl;
+    hEBFakeRate->SetBinContent(i+1, EBFake[i]);
+    hEBFakeRate->SetBinError(i+1, EBFakeE[i]);
+    std::cout << "EE FakeRate " << i << ": " << EEFake[i]  << " +/- " << EEFakeE[i] << std::endl;
+    hEEFakeRate->SetBinContent(i+1, EEFake[i]);
+    hEEFakeRate->SetBinError(i+1, EEFakeE[i]);
+  }
+
+  cEBFakeRate->cd(); hEBFakeRate->Draw();
+  cEEFakeRate->cd(); hEEFakeRate->Draw();
+
+  hEBFakeRate->SetMinimum(0);  hEEFakeRate->SetMinimum(0);
+  hEBFakeRate->SetMaximum(0.2);  hEEFakeRate->SetMaximum(0.6);
+
+  // Measure Fake Rate Underestimation or Overestimation
+  // Ratio of the fake rate with the known MC truth as a function of pT
   TCanvas *c1=new TCanvas("cEBRatio","EBRatio");
   TCanvas *c2=new TCanvas("cEERatio","EERatio");
   TH1D* hEBRatio=new TH1D("hEBRatio","EB Ratio", NPTBINS, PTBINS);
@@ -236,6 +263,7 @@ void fit(void)
     hEBRatio->SetBinContent(i+1, r);
     hEBRatio->SetBinError(i+1, e);
   }
+
   for(int i=0; i<NPTBINS; i++) {
     double r=EEFake[i]/EETruth[i];
     double e1=EEFakeE[i]/EEFake[i];
@@ -251,5 +279,25 @@ void fit(void)
 
   hEBRatio->SetMinimum(0);  hEERatio->SetMinimum(0);
   hEBRatio->SetMaximum(2.0);  hEERatio->SetMaximum(2.0);
+
+  // Save Plots and Histograms
+  TList *l = new TList();
+  l->Add(hEBRatio);
+  l->Add(hEERatio);
+  TFile *f = new TFile("fake_rate_sieie_template_fits.root","RECREATE");
+  l->Write("histlist", TObject::kSingleKey);
+  f->ls();
+
+  // Fake Rate
+  c1->SaveAs("fake_rate_EBRatio.pdf");
+  c2->SaveAs("fake_rate_EERatio.pdf");
+  cEBFakeRate->SaveAs("fake_rate_FakeTemplatesEB.pdf");
+  cEEFakeRate->SaveAs("fake_rate_FakeTemplatesEE.pdf");
+
+  cEB->SaveAs("fake_rate_cEB.pdf");
+  cEE->SaveAs("fake_rate_cEE.pdf");
+  cEBTruth->SaveAs("fake_rate_cEBTruth.pdf");
+  cEETruth->SaveAs("fake_rate_cEETruth.pdf");
+
   return;
 }
