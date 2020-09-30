f32be9834d034b746c18d2c5f586f2ac37a3c728
Chris West <christopher.alan.west@gmail.com>
2020-09-29 20:57:26 -0500

    Correct file name

--- Tools/interface/fakePrediction.h
+++ Tools/interface/fakePrediction.h
@@ -4,7 +4,7 @@
 // needed to get default implementations from base class
 #define ntupleAnalyzerBase_cxx
 
-#include "diphoton-analysis/Tools/interface/fakeRates.h"
+#include "diphoton-analysis/Tools/interface/fakeRates.hh"
 
 #include "ntupleAnalyzerBase.h"
 // only for use with legacy ntuples
