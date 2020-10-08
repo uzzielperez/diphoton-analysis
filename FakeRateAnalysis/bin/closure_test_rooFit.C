82c00789ca4d9b575424e85eb6e9c8c42396910f
Uzziel <uzzie.perez@cern.ch>
2020-10-08 10:23:04 -0500

    rooFit

--- FakeRateAnalysis/bin/closure_test_rooFit.C
+++ FakeRateAnalysis/bin/closure_test_rooFit.C
@@ -148,6 +148,7 @@ int main(int argc, char *argv[])
     bkgvsptEE->GetXaxis()->SetTitle("p_{T} (GeV)");
     bkgVsPtEEVec.push_back(bkgvsptEE);
   }
+
   TString pvCut = "";
   if(pvCutLow!=0 || pvCutHigh!=2000) pvCut = Form("_nPV%i-%i", pvCutLow, pvCutHigh);
   TFile outfile("fakeRatePlots_" + sample + "_" + era + pvCut + ".root","recreate");
